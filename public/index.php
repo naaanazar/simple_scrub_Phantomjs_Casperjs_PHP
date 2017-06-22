5c82ec95782082603c557cbf0569dce43dd1398c
Nazar Korchevsky <naaa.nazar@gmail.com>
2017-06-22 10:15:55 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ if(isset($_POST['search'])){
 
     putenv('PHANTOMJS_EXECUTABLE=C:\Users\naaanazar\OpenServer\domains\parser\vendor\phantomjs\bin\phantomjs.exe');
     $command = exec('C:\Users\naaanazar\OpenServer\domains\parser\vendor\casperjs\bin\casperjs.exe ..\leboncoin.js '. $_POST['search'], $res1);
+//    putenv('PHANTOMJS_EXECUTABLE=C:\Users\naaanazar\OpenServer\domains\parser\vendor\phantomjs\bin\phantomjs.exe');
+//    $command = exec('C:\Users\naaanazar\OpenServer\domains\parser\vendor\casperjs\bin\casperjs.exe ..\leboncoin.js '. $_POST['search'], $res1);
     $view = '../src/views/results.phtml';
     include('../src/views/tamplate.phtml');
     exit();
@@ -13,6 +15,8 @@ if(isset($_POST['send'])){
     set_time_limit ( 1000 );
     putenv('PHANTOMJS_EXECUTABLE=C:\Users\naaanazar\OpenServer\domains\parser\vendor\phantomjs\bin\phantomjs.exe');
     $command = exec('C:\Users\naaanazar\OpenServer\domains\parser\vendor\casperjs\bin\casperjs.exe ..\sendMassages.js '. $_POST['send'], $res2);
+//    putenv('PHANTOMJS_EXECUTABLE=C:\Users\naaanazar\OpenServer\domains\parser\vendor\phantomjs\bin\phantomjs.exe');
+//    $command = exec('C:\Users\naaanazar\OpenServer\domains\parser\vendor\casperjs\bin\casperjs.exe ..\sendMassages.js '. $_POST['send'], $res2);
     $view = '../src/views/send_success.phtml';
 
     include('../src/views/tamplate.phtml');
